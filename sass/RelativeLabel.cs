77c2218776c2a4bc633b720fd1ab7e8aa5fd74aa
Drew DeVault <sir@cmpwn.com>
2013-03-04 14:39:24 -0700

    Added missing file

---
+++ sass/RelativeLabel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sass
+{
+    public class RelativeLabel
+    {
+        public int RootLineNumber { get; set; }
+        public uint Address { get; set; }
+    }
+}
