5b5def0cb55865b831ed5655d6934553a444404c
Drew DeVault <sir@cmpwn.com>
2014-01-30 22:18:23 -0800

    Rearrange undocumented IX/IY(H,L) instructions

--- sass/test.asm
+++ sass/test.asm
@@ -1,17 +1,1 @@
-﻿.macro setBankA(page)
-    .if page & 0x80
-        .echo "high"
-        ld a, 1
-        out (0x0E), a
-        ld a, page & 0x7F
-        out (6), a
-    .else
-        .echo "low"
-        xor a
-        out (0x0E), a
-        ld a, page & 0x7F
-        out (6), a
-    .endif
-.endmacro
-
-setBankA(4)
+﻿ld a, ixl
